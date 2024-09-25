0d983e4e023288e1179a2bac5999a726aac9e03c
sribabu <sribabumandraju@gmail.com>
2024-09-25 13:36:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import './index.css'
+
+const App = () => {
+  return (
+    <div className="flex items-center w-full h-screen bg-[black] justify-center">
+      <div className="text-[100px] font-bold text-[aqua]">Tailwind CSS Template</div>
+    </div>
+  )
+}
+
+export default App
